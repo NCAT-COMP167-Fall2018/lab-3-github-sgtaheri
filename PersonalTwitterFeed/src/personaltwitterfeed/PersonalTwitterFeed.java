324d788117ff1e4d2907b42b9ffa713653aece9f
Sogol Ghotbi Taheri <sghotbitaheri@ncat.edu>
2018-09-10 18:00:58 -0400

    stuff

--- PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
+++ PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
@@ -26,16 +26,49 @@ public class PersonalTwitterFeed {
         System.out.println("Welcome to your personal Twitter!");
         System.out.println("What's your name, tweeter?");
         
+       
         String tweeterName = keyboard.nextLine();
         
         System.out.println("Nice to meet you " + tweeterName + "!");
         System.out.println("Enter your tweets and I will add them to your timeline!");
         
         int numTweets = 0;
-        
+        //newTweet
         while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+            tweets[numTweets] = keyboard.nextLine() + getTimeStamp();
+            numTweets++;
+            
+            System.out.println(tweeterName + "'s Personal Twitter Feed:");
+            for(int i = 0; i < numTweets; i++) {
+                System.out.println("- " + tweets[i]);
+            }
+            
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            
+            if(numTweets < (MAX_NUMBER_TWEETS - 1))
+                System.out.println("Enter your next tweet:");
+        }
+        newTweet(tweets);
+        System.out.println("Your twitter feed is full");
+    }
+    public static String getTimeStamp (){
+        Date now = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat(" yyyy-MM-dd HH:mm:ss.SSSZ");
+        return dateFormat.format(now);
+    }
+    public static void newTweet(String[] tweets) {
+        int numTweets = 0;
+        //newTweet
+        while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+            Scanner keyboard = new Scanner(System.in);
             tweets[numTweets] = keyboard.nextLine();
             numTweets++;
+            String tweeterName;
+            tweeterName = keyboard.nextLine();
+            System.out.println("Nice to meet you " + tweeterName + "!");
             
             System.out.println(tweeterName + "'s Personal Twitter Feed:");
             for(int i = 0; i < numTweets; i++) {
@@ -52,6 +85,6 @@ public class PersonalTwitterFeed {
         }
         
         System.out.println("Your twitter feed is full");
+        
     }
-    
-}
+            }
